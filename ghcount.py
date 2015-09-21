2ef9b3a9d64d7b4e214d646a6e5292073cbba0e0
Yanqiang(Oliver) Luan <bk13lyq@126.com>
2015-09-21 14:47:37 +0800

    增加一列统计项：每个作者的commit次数。

--- ghcount.py
+++ ghcount.py
@@ -16,19 +16,21 @@ authorList = []
 try:
     opts, args = getopt.getopt(sys.argv[1:],"",["author=","committer="])
 except getopt.GetoptError:
-    print 'Wrong input!! \nUsage:\nghcount.py -i <inputfile> -o <outputfile> --author=<comma separated author names>'
+    print 'Wrong input!! \nUsage:\nghcount.py [--author=<comma separated author names>]'
     sys.exit(2)
 for opt, arg in opts:
     if opt == '-h':
-        print 'Usages:\nghcount.py -i <inputfile> -o <outputfile> --author=<comma separated author names>'
+        print 'Usages:\nghcount.py [--author=<comma separated author names>]'
         sys.exit()
     elif opt in ("--committer", "--author"):
         authors = arg
 
 authorList = authors.split(",")
-print authorList
+
+# debugging for input option: authorList
+#print authorList
 print 'collecting git log...'
-(exitstatus, outtext) = commands.getstatusoutput('git log --numstat --first-parent')
+(exitstatus, outtext) = commands.getstatusoutput('git log --numstat')
 
 if exitstatus != 0:
     print 'Error! Check if you\'re in a git repository.'
@@ -47,7 +49,9 @@ for line in lines:
         author = line[8:]
         if line[8:] not in profiles.viewkeys():
             # it's a new author, make new map item
-            profiles[author] = [0, 0, set()]
+            profiles[author] = [0, 0, set(), 1]
+        else:
+            profiles[author][3] += 1
     elif line[0].isdigit() and not line.endswith('pbxproj\n'):
         # print(line)
         segments = line.split('\t')
@@ -56,7 +60,7 @@ for line in lines:
         profiles[author][2].add(segments[2])
 
 for (k, v) in profiles.items():
-    print ('{:<40}: insertions: {:10}, deletions: {:10}, files: {:5}').format(k, str(v[0]), str(v[1]), str(len(v[2])))
+    print ('{:<40}: insertions: {:10}, deletions: {:10}, files: {:5}, commits: {:5}').format(k, str(v[0]), str(v[1]), str(len(v[2])), str(v[3]))
 
 if authors != '':
     print "\nIndividual code statistics:\n"
